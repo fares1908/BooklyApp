1055d842b7782e713a52468cc56d54399918593b
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-07-09 20:26:26 +0300

    animation

--- lib/Splash_veiw/presentation/view_models/views/widgets/splash_view_body.dart
+++ lib/Splash_veiw/presentation/view_models/views/widgets/splash_view_body.dart
@@ -1,8 +1,28 @@
+import 'package:bookly_app/Splash_veiw/presentation/view_models/views/widgets/sliding_text.dart';
 import 'package:bookly_app/core/utils/assetsdata.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation=Tween<Offset>(begin: const Offset(0,11),end: Offset.zero).animate(animationController);
+    animationController.forward();
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +30,14 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(
-         AssetsData.log
-        ),
-        const SizedBox(
-          height: 6,
-        ),
-        const Text('Read Free Books',
-        textAlign: TextAlign.center,),
+        Image.asset(AssetsData.log),
+         SizedBox(
+           height: 6,
+         ),
+         SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
 }
+
+
