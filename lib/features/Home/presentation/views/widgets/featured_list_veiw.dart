729a718f22b57887b98ab9e090dafda6f1d74e41
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-07-10 19:21:24 +0300

    solid

--- lib/features/Home/presentation/views/widgets/featured_list_veiw.dart
+++ lib/features/Home/presentation/views/widgets/featured_list_veiw.dart
@@ -9,7 +9,7 @@ class FutureBooksListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height*.3,
+      height: MediaQuery.of(context).size.height*.1,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
 
