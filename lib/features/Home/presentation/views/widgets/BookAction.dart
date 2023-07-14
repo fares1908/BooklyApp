f45a716ef964035ac6a3ffe88a5a33951f097f64
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-07-14 16:10:45 +0300

    button

---
+++ lib/features/Home/presentation/views/widgets/BookAction.dart
@@ -0,0 +1,43 @@
+import 'package:bookly_app/core/widgets/custom_button.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class  BookAction extends StatelessWidget {
+  const BookAction({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * .05),
+      child: Row(
+        children: [
+          Expanded(
+            child: CustomButton(
+              // fontSize:16 ,
+                text: '19.99\$',
+                textColor: Colors.black,
+                backgroundColor: Colors.white,
+                borderRadius: BorderRadius.only(
+                    bottomLeft:  Radius.circular(14),
+                    topLeft: Radius.circular(14)
+                )
+            ),
+          ),
+          Expanded(
+            child: CustomButton(
+                text: 'Free preview',
+                textColor: Colors.white,
+                backgroundColor: Color(0xffEf8262),
+                borderRadius: BorderRadius.only(
+                    bottomRight:  Radius.circular(14),
+                    topRight: Radius.circular(14)
+                )
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/Home/presentation/views/widgets/book_detailes_view_body.dart
+++ lib/features/Home/presentation/views/widgets/book_detailes_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:bookly_app/core/utils/assetsdata.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/core/widgets/custom_button.dart';
+import 'package:bookly_app/features/Home/presentation/views/widgets/BookAction.dart';
 import 'package:bookly_app/features/Home/presentation/views/widgets/CustomBookDetailsAppBar.dart';
 import 'package:bookly_app/features/Home/presentation/views/widgets/ListViewItem.dart';
 import 'package:bookly_app/features/Home/presentation/views/widgets/book_detailes_view_body.dart';
@@ -14,41 +16,97 @@ class BookDetailsViewBody extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
-
           children: [
             CustomBookDetailsAppBar(),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*.26),
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * .26),
               child: ListViewItemImage(),
             ),
             SizedBox(
               height: 34,
             ),
-
-            Text('The Jungle Book',
-            style: Styles.textStyle30,
+            Text(
+              'The Jungle Book',
+              style: Styles.textStyle30,
             ),
             SizedBox(
               height: 6,
             ),
             Opacity(
               opacity: .7,
-              child: Text('Rudyard Kipling',
+              child: Text(
+                'Rudyard Kipling',
                 style: Styles.textStyle18.copyWith(
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500
-                ),
+                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
               ),
             ),
             SizedBox(
               height: 18,
             ),
+            SizedBox(width: 120, child: BookingRate()),
             SizedBox(
-              width: 120,
-                child: BookingRate()),
+              height: 37,
+            ),
+            BookAction()
+
           ],
         ),
       ),
     );
   }
 }
+// class CustomButton extends StatelessWidget {
+//   const CustomButton({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: EdgeInsets.symmetric(
+//           horizontal: MediaQuery.of(context).size.width * .07),
+//       child: Row(
+//         children: [
+//           Expanded(
+//             child: Container(
+//                 decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.only(
+//                       topLeft: Radius.circular(16),
+//                       bottomLeft: Radius.circular(16)),
+//                   color: Colors.white,
+//                 ),
+//                 child: MaterialButton(
+//                   onPressed: () {},
+//                   child: Text(
+//                     '19.99\$',
+//                     style: Styles.textStyle16.copyWith(
+//                         color: Colors.black,
+//                         fontWeight: FontWeight.bold),
+//                   ),
+//                 )),
+//           ),
+//           Expanded(
+//             child: Container(
+//                 decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.only(
+//                       topRight: Radius.circular(16),
+//                       bottomRight: Radius.circular(16)),
+//                   color: Color(0xffEf8262),
+//                 ),
+//                 child: MaterialButton(
+//                   onPressed: () {},
+//                   child: Text(
+//                     'Free preview',
+//                     style: Styles.textStyle16.copyWith(
+//
+//                         fontWeight: FontWeight.bold),
+//                   ),
+//                 )),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+
