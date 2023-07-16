689be5affa006ebdddefc6cf614ae95a54465f77
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-07-16 13:23:12 +0300

    fetch newest book

--- lib/features/Home/data/repositories/home_repo_imple.dart
+++ lib/features/Home/data/repositories/home_repo_imple.dart
@@ -1,19 +1,31 @@
 import 'package:bookly_app/core/errors/Failur.dart';
+import 'package:bookly_app/core/utils/api_services.dart';
 import 'package:bookly_app/features/Home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/Home/data/repositories/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl implements HomeRepo{
-  @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+class HomeRepoImpl implements HomeRepo {
+  final ApiServices apiServices;
 
-    throw UnimplementedError();
+  HomeRepoImpl(this.apiServices);
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoint: '?Filtering=free-ebooks&Sorting=newest &q=subject:sport');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerError());
+    }
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-
     throw UnimplementedError();
   }
-
 }
