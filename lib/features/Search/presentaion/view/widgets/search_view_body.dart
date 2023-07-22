eddd8bb86c9e98fe6b655f19e521d9e90ade234c
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-07-22 16:59:02 +0300

    implement fetchSimilarBooks

--- lib/features/Search/presentaion/view/widgets/search_view_body.dart
+++ lib/features/Search/presentaion/view/widgets/search_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/Home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/Home/presentation/views/widgets/NewestListVeiwItem.dart';
+import 'package:bookly_app/features/Search/presentaion/view/widgets/SearchResultSection.dart';
 import 'package:bookly_app/features/Search/presentaion/view/widgets/custom_search_text_field.dart';
 import 'package:bookly_app/features/Search/presentaion/view/widgets/search_list_view.dart';
 import 'package:flutter/material.dart';
@@ -12,27 +13,29 @@ import '../../../../../core/utils/routes.dart';
 import '../../../../Home/presentation/views/widgets/ListViewItem.dart';
 
 class SearchViewBody extends StatelessWidget {
-  const SearchViewBody({super.key, required this.bookModel});
-  final BookModel bookModel;
+  const SearchViewBody({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 15),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
           SizedBox(
             height: 13,
           ),
-          Text('Result',
-          style: Styles.textStyle20,
+          Text(
+            'Result',
+            style: Styles.textStyle20,
           ),
           SizedBox(
-            height: 13,
+            height: 15,
           ),
-          Expanded(child: SearchResultListView(bookModel: bookModel,)),
+          Expanded(child: SearchResultSection())
+
         ],
       ),
     );
