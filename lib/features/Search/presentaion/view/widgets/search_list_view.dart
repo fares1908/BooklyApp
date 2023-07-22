16dab4bc37c292b8d717dec9553a4710698c6813
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-07-22 16:58:35 +0300

    implement fetchSimilarBooks

--- lib/features/Search/presentaion/view/widgets/search_list_view.dart
+++ lib/features/Search/presentaion/view/widgets/search_list_view.dart
@@ -1,5 +1,9 @@
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/features/Search/presentaion/manger/search_books/search_cubit.dart';
+import 'package:bookly_app/features/Search/presentaion/view/widgets/search_listview_item.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../../CONSTANCE.dart';
@@ -8,79 +12,50 @@ import '../../../../../core/utils/styles.dart';
 import '../../../../Home/data/models/book_model/book_model.dart';
 
 class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key, required this.bookModel});
-  final BookModel bookModel;
+  const SearchResultListView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        shrinkWrap: true,
-        // physics: const NeverScrollableScrollPhysics(),
-        itemBuilder: (context, index) =>GestureDetector(
-          onTap: () {
-            GoRouter.of(context).push(AppRouter.kDetailsView);
-          },
-          child: SizedBox(
-            // height: MediaQuery.of(context).size.height * .2,
-            height: 125,
-            child: Row(
-              // crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                AspectRatio(
-                  aspectRatio: 2.5 / 4,
-                  child: CachedNetworkImage(
-                      imageUrl: bookModel.volumeInfo.imageLinks!.thumbnail
-                  )
-                ),
-                const SizedBox(
-                  width: 4,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * .5,
-                      child: Text(
-                        '${bookModel.volumeInfo.title}',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style:
-                        Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Text(
-                      '${bookModel.volumeInfo.publisher}',
-                      style: Styles.textStyle14.copyWith(color: Colors.grey),
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * .6,
-                      child: Row(
-                        children: [
-                          Text('Free',
-                              style: Styles.textStyle20
-                                  .copyWith(fontWeight: FontWeight.bold)),
-                          Spacer(),
-
-                        ],
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
-        separatorBuilder: (context, index) {
-          return Container(
-            width: double.infinity,
-          );
+    return SizedBox(
+      // height: context.size!.height * .3,
+      child: BlocBuilder<SearchCubit, SearchState>(
+        buildWhen: (previous, current) {
+          if (current is SearchILoading ||
+              current is SearchSuccess ||
+              current is SearchError) {
+            return true;
+          }
+          return false;
         },
-        itemCount: 10);
+        builder: (context, state) {
+          if (state is SearchSuccess) {
+            return ListView.separated(
+              scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                // physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SearchListViewItem(
+                    bookModel: state.books[index],
+                  ),
+                ),
+                separatorBuilder: (context, index) {
+                  return Container(
+                    width: double.infinity,
+                  );
+                },
+                itemCount: state.books.length);
+          } else if(state is SearchError){
+            return CustomErrorWidget(errorMessage: state.errorMessage);
+          }else {
+            // return ListView.builder(
+            //   scrollDirection: ,
+            //     itemBuilder:(context, index) => ,
+            // );
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 }
